4024eb131792a5b5be7b2a50eff1f88d55628343
kelechichukwu74-byte <kelechichukwu74@gmail.com>
2026-09-22 10:17:17 +0100

    Update order.js

--- api/order.js
+++ api/order.js
@@ -8,8 +8,7 @@ const SUPABASE_URL =
   "https://rfitbmkizfmwfqqskwhy.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  process.env.SUPABASE_ANON_KEY ||
-  process.env.SUPABASE_PUBLISHABLE_KEY;
+  "sb_publishable_erjKhsDOoyhbHDExv7Q_gpGcK0C-";
 
 const SUPABASE_SERVICE_ROLE_KEY =
   process.env.SUPABASE_SERVICE_ROLE_KEY;
